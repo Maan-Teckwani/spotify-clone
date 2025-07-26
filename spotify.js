72a6f63ae1161d63b86dce6341997217c7daf295
Maan Teckwani <maanteckwani@gmail.com>
2025-07-26 10:58:12 +0530

    initial commit

---
+++ spotify.js
@@ -0,0 +1,5 @@
+
+document.getElementById("greetButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Hello, welcome to my website!";
+    console.log("Button clicked!");
+});
